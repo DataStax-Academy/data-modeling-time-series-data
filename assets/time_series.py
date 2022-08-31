ebab6aa591e5370344fafc748280f22e0801aeb0
Artem Chebotko <artemchebotko@gmail.com>
2022-08-31 17:54:22 -0500

    added assets

---
+++ assets/time_series.py
@@ -0,0 +1,127 @@
+from datetime import datetime
+from datetime import timedelta
+from random import randrange
+from random import uniform
+import numpy as np
+
+
+
+def populate_sources_by_group():
+    print("""
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House A','Termostate A1', 
+        {'Model number':'EB-STATE5-01','Min temperature':'45F','Max temperature':'92F','Temperature sensitivity':'1F',
+        'Min humidity':'5%','Max humidity':'95%','Humidity sensitivity':'1%'},
+        'primary residence');
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House A','Termostate A2', 
+        {'Model number':'EB-STATE5-01','Min temperature':'45F','Max temperature':'92F','Temperature sensitivity':'1F',
+        'Min humidity':'5%','Max humidity':'95%','Humidity sensitivity':'1%'},
+        'primary residence');       
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House A','Refrigerator A1', 
+        {'Model number':'RF28R7551SG','Capacity':'15.6 cubic feet'},
+        'primary residence');         
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House A','Freezer A1', 
+        {'Model number':'RF28R7551SG','Capacity':'8.3 cubic feet'},
+        'primary residence');         
+        
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House B','Termostate B1', 
+        {'Model number':'EB-STATE5-01','Min temperature':'45F','Max temperature':'92F','Temperature sensitivity':'1F',
+        'Min humidity':'5%','Max humidity':'95%','Humidity sensitivity':'1%'},
+        'vacation home');
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House B','SmartSensor B1', 
+        {'Model number':'EB-RSHM2PK-01'},
+        'vacation home'); 
+INSERT INTO sources_by_group 
+(group,source,characteristics,description)
+VALUES ('House B','SmartSensor B2', 
+        {'Model number':'EB-RSHM2PK-01'},
+        'vacation home');                                   
+    """);
+
+
+def populate_metrics():
+    print("""
+INSERT INTO metrics (bucket,metric,unit)
+VALUES ('all','temperature','Fahrenheit');
+INSERT INTO metrics (bucket,metric,unit)
+VALUES ('all','humidity','% Relative Humidity');
+INSERT INTO metrics (bucket,metric,unit)
+VALUES ('all','occupancy','True/False or 1/0');                                   
+    """);    
+    
+
+def populate_time_series(group,source,metric,min_value,max_value):
+    latest_timestamp = datetime.now().replace(second=0, microsecond=0)
+    midnight_timestamp = latest_timestamp.replace(minute=0, hour=0)
+    resolution_60s = timedelta(seconds=60) # High resolution - every 60 seconds
+    resolution_60m = timedelta(minutes=60) # Low resolution - every 60 minutes
+    num_points_high = 10 * 24 * 60  # Minutes in 10 days - number of high resolution points        
+    num_points_low = 2 * 365 * 24 # Hours in 2 years - number of low resolution points
+    points_60s = []
+    points_60m = []
+    # Compute high resolution points
+    for i in range(0,num_points_high):
+        point = randrange(min_value, max_value + 1, 1)
+        points_60s.append( point )
+        if ((i + 1) % 60) == 0:
+            points_60m.append( round( np.mean( points_60s[i - 60 + 1 : i + 1] ), 2 ) )
+    # Compute low resolution points        
+    for i in range(len(points_60m),num_points_low):
+        point = round( uniform(min_value, max_value), 2 )
+        points_60m.append( point )            
+    # Populate high resolution tables
+    for i in range(0,num_points_high):
+        point = points_60s[i]      
+        timestamp = latest_timestamp - i * resolution_60s      
+        print("INSERT INTO series_by_source_high (group,source,timestamp,metric,value) VALUES (" + "'" + group + "'," + "'" + source + "'," + "'" + str(timestamp) + "'," + "'" + metric + "'," + str(point) + ");")
+        print("INSERT INTO series_by_metric_high (group,source,timestamp,metric,value) VALUES (" + "'" + group + "'," + "'" + source + "'," + "'" + str(timestamp) + "'," + "'" + metric + "'," + str(point) + ");")
+    # Populate low resolution tables
+    for i in range(0,num_points_low):
+        point = points_60m[i]     
+        timestamp = latest_timestamp - i * resolution_60m       
+        print("INSERT INTO series_by_source_low (group,year,source,timestamp,metric,value) VALUES (" + "'" + group + "'," + str(timestamp.year) + "," + "'" + source + "'," + "'" + str(latest_timestamp.replace(minute=0) - i * resolution_60m) + "'," + "'" + metric + "'," + str(point) + ");")
+        print("INSERT INTO series_by_metric_low (group,year,source,timestamp,metric,value) VALUES (" + "'" + group + "'," + str(timestamp.year) + "," + "'" + source + "'," + "'" + str(latest_timestamp.replace(minute=0) - i * resolution_60m) + "'," + "'" + metric + "'," + str(point) + ");")
+    # Populate the table with statistics
+    num_points_ignore = int((latest_timestamp - midnight_timestamp).total_seconds()) / int(resolution_60m.total_seconds())
+    for i in range(num_points_ignore,num_points_low,24):
+        if (i+24) > num_points_low:
+            break;
+        points = points_60m[i : i + 24]
+        points.sort()
+        min = points[0]
+        max = points[23]
+        median = points[12]
+        mean = round( np.mean( points ), 2 )
+        std = round( np.std( points ), 2 )
+        timestamp = latest_timestamp.replace(minute=0) - i * resolution_60m      
+        date = str(timestamp.date()) 
+        print("INSERT INTO statistics_by_source_metric (source,metric,date,min,max,median,mean,stdev) VALUES (" + "'" + source + "'," + "'" + metric + "'," + "'" + date + "'," + str(min) + "," + str(max) + "," + str(median) + "," + str(mean) + "," + str(std) + ");")
+
+                
+        
+populate_sources_by_group()
+populate_metrics()        
+    
+populate_time_series('House A','Termostate A1','temperature',74,76)
+populate_time_series('House A','Termostate A1','humidity',40,50)
+populate_time_series('House A','Termostate A2','temperature',74,76)  
+populate_time_series('House A','Termostate A2','humidity',40,50)  
+populate_time_series('House A','Refrigerator A1','temperature',30,50)
+populate_time_series('House A','Refrigerator A1','humidity',45,75)   
+populate_time_series('House A','Freezer A1','temperature',-20,5)
+
+populate_time_series('House B','Termostate B1','temperature',80,82)
+populate_time_series('House B','SmartSensor B1','temperature',79,82)
+populate_time_series('House B','SmartSensor B2','temperature',82,85)      
+
